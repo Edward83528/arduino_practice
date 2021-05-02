985e76f5f576a5a5dcc29aec331ee813e80e015c
Edward830528 <u0151051@gmail.com>
2021-05-02 10:59:54 +0800

    format

--- 06.按鈕開關計數顯示在序列埠/sketch_may01b.ino
+++ 06.按鈕開關計數顯示在序列埠/sketch_may01b.ino
@@ -1,24 +1,24 @@
-const int BUTTON_PIN = 7;  // 按鍵的接腳
-const int LED_PIN =  3;   // LED燈的接腳
+const int BUTTON_PIN = 7; // 按鍵的接腳
+const int LED_PIN = 3; // LED燈的接腳
 
-int buttonState = 0;   // 按鈕的狀態
-int count = 0;   // 累加數值
+int buttonState = 0; // 按鈕的狀態
+int count = 0; // 累加數值
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // 設定按鈕的接腳為輸入，因為我們要讀取它的狀態
-  pinMode(LED_PIN, OUTPUT);   // 設定LED的PIN腳為輸出
+    Serial.begin(9600);
+    pinMode(BUTTON_PIN, INPUT_PULLUP); // 設定按鈕的接腳為輸入，因為我們要讀取它的狀態
+    pinMode(LED_PIN, OUTPUT); // 設定LED的PIN腳為輸出
 }
 
 void loop() {
-  buttonState = digitalRead(BUTTON_PIN);  //讀取按鍵的狀態
-  //Serial.println(buttonState); // 不按:1 or 按:0
-  if(buttonState == LOW){          //如果按鍵按了
-    count++;
-    delay(100); // 防止彈跳過快
-    digitalWrite(LED_PIN, HIGH);   //LED就亮了 
-  }else{                           //如果按鍵是未按下
-    digitalWrite(LED_PIN, LOW);    //LED就不亮
-  }
-  Serial.println(count); // 印出累加數字
+    buttonState = digitalRead(BUTTON_PIN); //讀取按鍵的狀態
+    //Serial.println(buttonState); // 不按:1 or 按:0
+    if (buttonState == LOW) { //如果按鍵按了
+        count++;
+        delay(100); // 防止彈跳過快
+        digitalWrite(LED_PIN, HIGH); //LED就亮了 
+    } else { //如果按鍵是未按下
+        digitalWrite(LED_PIN, LOW); //LED就不亮
+    }
+    Serial.println(count); // 印出累加數字
 }
